029d83a20fe6f148b2906813cd6e1b59aa644e73
Klaus Umbach <klaus@uxix.de>
2017-08-26 14:33:34 +0200

    added parameters

--- install.sh
+++ install.sh
@@ -1,24 +1,53 @@
 #!/bin/bash -e
 
 export LANG=C
-export INSTALL_PATH="${HOME}/.minikube/bin/"
-mkdir -p ${INSTALL_PATH}
-PATH="${INSTALL_PATH}:${PATH}"
-echo "export PATH=\"${INSTALL_PATH}:\${PATH}\"" > minienv
+LATEST="false"
+START="false"
+MEMORY=2048
+CPUS=2
+DISK='20g'
 
-# get latest release on github
-minikube_version="$(curl --silent 'https://github.com/kubernetes/minikube/releases/latest' | sed 's!.*/releases/tag/\(v[0-9].*\)">.*!\1!')"
-dockermachine_version="$(curl --silent 'https://github.com/docker/machine/releases/latest' | sed 's!.*/releases/tag/\(v[0-9].*\)">.*!\1!')"
-kvm_driver_version="$(curl --silent 'https://github.com/dhiltgen/docker-machine-kvm/releases/latest' | sed 's!.*/releases/tag/\(v[0-9].*\)">.*!\1!')"
-# Dynamic
-kubectl_version="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
+usage () { # {{{
+    echo "usage: ${0} [-hlscdm]"
+    echo ""
+    echo " -h    this help"
+    echo " -l    use latest versions"
+    echo " -s    start/initialize your minikube afterwards"
+    echo " -c N  number of CPUs (default=${CPUS})"
+    echo " -m N  amount of memory (in MiB) to use (default=${MEMORY})"
+    echo " -d N  amount of diskspace to use (default=${DISK})"
+    echo ""
+} # }}}
 
-_minikube_url="https://storage.googleapis.com/minikube/releases/${minikube_version}/minikube-linux-amd64"
-_kubectl_url="https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl"
-_dockermachine_url="https://github.com/docker/machine/releases/download/${dockermachine_version}/docker-machine-Linux-x86_64"
-_kvm_driver_url="https://github.com/dhiltgen/docker-machine-kvm/releases/download/${kvm_driver_version}/docker-machine-driver-kvm-ubuntu16.04"
+while getopts "hlsm:c:d:" OPTION; do # {{{
+    case ${OPTION} in
+        h)
+            usage
+            exit 255
+        ;;
+        l)
+            LATEST="true"
+        ;;
+        s)
+            START="true"
+        ;;
+        m)
+            MEMORY=${OPTARG}
+        ;;
+        c)
+            CPUS=${OPTARG}
+        ;;
+        d)
+            DISK=${OPTARG}
+        ;;
+    esac
+done # }}}
 
-_download () {
+_latest_github_release () { # {{{
+    curl --silent "https://github.com/${1}/releases/latest" | sed 's!.*/releases/tag/\(v[0-9].*\)">.*!\1!'
+} # }}}
+
+_download () { # {{{
     local __name=$1
     local __version=$2
     local __url=$3
@@ -64,15 +93,46 @@ _download () {
 
     chmod +x ${INSTALL_PATH}/${__name}
     echo ""
-}
+} # }}}
+
+if [[ ${LATEST} == 'true' ]]; then
+    echo 'Getting latest versions of everything! \o/'
+    minikube_version="$(_latest_github_release kubernetes/minikube)"
+    dockermachine_version="$(_latest_github_release docker/machine)"
+    kvm_driver_version="$(_latest_github_release dhiltgen/docker-machine-kvm)"
+    kubectl_version="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
+else
+    echo 'Getting predefined versions of everything.'
+    minikube_version="v0.21.0"
+    dockermachine_version="v0.12.2"
+    kvm_driver_version="v0.10.0"
+    kubectl_version="v1.7.4"
+fi
+
+_minikube_url="https://storage.googleapis.com/minikube/releases/${minikube_version}/minikube-linux-amd64"
+_kubectl_url="https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl"
+_dockermachine_url="https://github.com/docker/machine/releases/download/${dockermachine_version}/docker-machine-Linux-x86_64"
+_kvm_driver_url="https://github.com/dhiltgen/docker-machine-kvm/releases/download/${kvm_driver_version}/docker-machine-driver-kvm-ubuntu16.04"
+
+export INSTALL_PATH="${HOME}/.minikube/bin/"
+mkdir -p ${INSTALL_PATH}
+PATH="${INSTALL_PATH}:${PATH}"
+echo "export PATH=\"${INSTALL_PATH}:\${PATH}\"" > minienv
+
 
 _download minikube ${minikube_version} ${_minikube_url}
 _download kubectl ${kubectl_version} ${_kubectl_url}
 _download docker-machine ${dockermachine_version} ${_dockermachine_url}
 _download docker-machine-driver-kvm ${kvm_driver_version} ${_kvm_driver_url}
 
-minikube start --vm-driver=kvm
+if [[ ${START} == 'true' ]]; then
+    echo "Starting minikube with ${MEMORY} MiB RAM, ${CPUS} CPUs and ${DISK} disk size:" 
+    minikube start --vm-driver=kvm --memory=${MEMORY} --cpus=${CPUS} --disk-size=${DISK}
+else
+    echo "create your minikube with:"
+    echo "$ minikube start --vm-driver=kvm --memory=${MEMORY} --cpus=${CPUS} --disk-size=${DISK}"
+fi
 
 echo "" 
-echo "run '. minienv' to enable your minikube-environment"
+echo "run '. minienv' to enable your minikube-environment in your shell"
 echo "" 
